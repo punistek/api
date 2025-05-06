ee3d5304468609e2783d5b725f83d2090639705e
punistek <104331040+punistek@users.noreply.github.com>
2025-05-06 21:03:57 +0300

    Update send.js

--- api/api/send.js
+++ api/api/send.js
@@ -1,18 +1,19 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send('Sadece POST istekleri desteklenir');
+  if (req.method !== 'GET') {
+    return res.status(405).send('Sadece GET istekleri desteklenir');
   }
 
-  const { key, msg } = req.body;
+  const { key, msg } = req.query;
 
+  // 1. Güvenlik anahtarı kontrolü
   const secretKey = 'ABC123';
   if (key !== secretKey) {
     return res.status(403).send('Yetkisiz erişim');
   }
 
-  const telegramToken = '7426XXXXXXXX:XXXXXXXXXXXX'; // Token gizli kalsın
+  // 2. Telegram'a gönderilecek mesaj
+  const telegramToken = '7426497726:AAEPDzRSsXjAvTFpN_B7bteQj00a6wacSAg';
   const chatId = '1224314188';
-
   const telegramUrl = `https://api.telegram.org/bot${telegramToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(msg)}`;
 
   try {
